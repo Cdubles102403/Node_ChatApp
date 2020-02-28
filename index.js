13d50a305a9061d0fa5049d4296403be8e137e92
cdubles <33189090+cdubles@users.noreply.github.com>
2020-02-28 16:30:54 -0500

    add sanatization for user input
    - sanatize user input to help with xss

--- index.js
+++ index.js
@@ -9,7 +9,22 @@ app.use(express.static('views'))
 io.on('connection', function (socket) {
   console.log("user connected " + socket.id)
   socket.on("message", function(msg){
-    console.log(socket.id+" message: "+msg);
-    socket.broadcast.emit("msg",msg)
+    message = sanitize(msg);
+    console.log(socket.id+" message: "+message)
+    socket.broadcast.emit("msg",message)
+    socket.emit("msg",message)
   });
 });
+//sanitize text to prevent xss
+function sanitize(string) {
+  const map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#x27;',
+      "/": '&#x2F;',
+  };
+  const reg = /[&<>"'/]/ig;
+  return string.replace(reg, (match)=>(map[match]));
+}
--- views/js.js
+++ views/js.js
@@ -1,14 +1,12 @@
 $(document).ready(function(){
-  var socket = io.connect('http://10.217.11.52:8080/');
+  var socket = io.connect('http://192.168.1.13:8080/');
 
   $("button#post").click(function(){
     console.log("clicked");
     var msg = $("input#message").val();
     socket.emit("message",msg);
-    $("div#messages").append("<br>"+msg);
   });
 socket.on("msg", function(msg){
-    $("div#messages").append("<br>"+msg);
-    
+    $("ul#messages").append("<li>"+msg+"<li>");
     });
 });
