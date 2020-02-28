54415284afabe6bc77fddbbf2c654cd8cfb7ea50
Cdubles102403 <44614396+Cdubles102403@users.noreply.github.com>
2020-02-28 09:00:55 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ server.listen(8080);
 app.use(express.static('views'))
 
 io.on('connection', function (socket) {
-  console.log("working")
+  console.log("user connected " + socket.id)
   socket.on("message", function(msg){
-    console.log(msg);
+    console.log(socket.id+" message: "+msg);
+    socket.broadcast.emit("msg",msg)
   });
 });
--- views/js.js
+++ views/js.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-  var socket = io.connect('http://localhost:8080/');
+  var socket = io.connect('http://10.217.11.52:8080/');
 
   $("button#post").click(function(){
     console.log("clicked");
     var msg = $("input#message").val();
     socket.emit("message",msg);
+    $("div#messages").append("<br>"+msg);
   });
+socket.on("msg", function(msg){
+    $("div#messages").append("<br>"+msg);
+    
+    });
 });
